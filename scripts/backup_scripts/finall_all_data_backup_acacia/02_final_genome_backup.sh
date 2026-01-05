10916e85eea5cfca3f4fdea83d91d5351a60bca0
Tyler Peirce <tyler.peirce@gmail.com>
2026-01-05 10:58:30 +0800

    updated to include backup to S3

--- scripts/backup_scripts/finall_all_data_backup_acacia/02_final_genome_backup.sh
+++ scripts/backup_scripts/finall_all_data_backup_acacia/02_final_genome_backup.sh
@@ -18,6 +18,12 @@
 for i in  "$rundir"/*; do
     if [ -d "$i" ]; then  # Check if the item in $rundir is a directory
         OG=$(basename "$i")
+
+        # Copy the fastq files and final assembly to the s3 bucket
+        S3=s3://oceanomics/OceanGenomes/analysed-data/draft-genomes
+        rclone copy "$i/fastp/" "$S3/$OG"/ --include "*.fastq.gz" --checksum -P
+        rclone copy "$i/assemblies/genome/" "$S3/$OG"/ --include "*.fna" --checksum -P
+
         # Find the .meryl directory using 'find'
         MERYL_DIR=$(find "$i/kmers" -maxdepth 1 -type d -name "*.meryl" | head -n 1)
         echo "meryl $MERYL_DIR"
